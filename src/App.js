25fed26b5869f986f8587433a8a7caf310b30585
Sarah <sarahkane682@yahoo.com>
2022-09-30 22:18:05 -0700

    changed to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Aprender from "./Pages/Aprender";
 import Home from "./Pages/Home";
 import Somo from "./Pages/Somo";
